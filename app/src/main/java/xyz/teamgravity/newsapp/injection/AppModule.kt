ec2acf8f3c1fca663c351a7d74e677d72bdf9cce
Raheem <raheemideadev@gmail.com>
2021-01-22 18:25:52 +0500

    raheem

---
+++ app/src/main/java/xyz/teamgravity/newsapp/injection/AppModule.kt
@@ -0,0 +1,12 @@
+package xyz.teamgravity.newsapp.injection
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+
+}
