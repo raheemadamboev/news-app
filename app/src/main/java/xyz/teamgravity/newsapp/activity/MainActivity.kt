a97f417142e925a585e02795bbcc825a4502b9d6
Raheem <raheemideadev@gmail.com>
2021-01-22 18:58:44 +0500

    raheem
    Took 5 minutes

--- app/src/main/java/xyz/teamgravity/newsapp/activity/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/newsapp/activity/MainActivity.kt
@@ -1,17 +1,33 @@
 package xyz.teamgravity.newsapp.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import xyz.teamgravity.newsapp.R
 import xyz.teamgravity.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.apply {
+            // bottom navigation view
+            bottomNavigationView.setupWithNavController(navController)
+
+            // do not respond if it again selected
+            bottomNavigationView.setOnNavigationItemReselectedListener { }
+        }
     }
 }
